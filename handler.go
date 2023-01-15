d3f49b1ec9c4e1a9dfae94e141a2ff6227a5708a
Jesse Gerard Brands <jessebrands@icloud.com>
2023-01-15 13:32:34 +0100

    minor refactoring and httpx.Request type

--- handler.go
+++ handler.go
@@ -5,38 +5,37 @@ import (
 	"net/http"
 )
 
-type HandlerFunc[T any] func(request *http.Request, body *T) *Response
+type HandlerFunc[T any] func(request *Request[T]) *Response
 
-func Handler[RequestBody any, ResponseBody any](handlerFunc HandlerFunc[RequestBody]) http.HandlerFunc {
+// Handler creates a http.HandlerFunc which wraps a HandlerFunc in some common boilerplate.
+func Handler[Req any, Res any](handlerFunc HandlerFunc[Req]) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
-			body               = new(RequestBody)
-			response *Response = nil
-		)
-		// decode the body and call handler func
-		err := json.NewDecoder(r.Body).Decode(&body)
-		if err != nil {
-			response = handlerFunc(r, nil)
-		} else {
-			response = handlerFunc(r, body)
+		request := &Request[Req]{
+			Request: r,
 		}
+		response := handlerFunc(request)
 		// this should never happen, and if it does, we should return 500
 		if response == nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 		w.WriteHeader(response.StatusCode)
-		_, _ = w.Write(transformBody[ResponseBody](response.Body))
+		_, _ = w.Write(transformBodyToBytes[Res](response.Body))
 	}
 }
 
-func transformBody[T any](body any) []byte {
+// transformBodyToBytes will transform body to a byte array.
+//
+// Internally this function type-checks body. A byte array will simply be returned, a string will be turned into bytes
+// and body is of type T or a pointer to T, it will be encoded as JSON. Any other value is considered an invalid type
+// and will be return an empty byte array.
+func transformBodyToBytes[T any](body any) []byte {
 	switch v := body.(type) {
 	case []byte:
 		return v
 	case string:
 		return []byte(v)
-	case T:
+	case T, *T:
 		if b, err := json.Marshal(v); err == nil {
 			return b
 		}
--- request.go
+++ request.go
@@ -1,8 +1,17 @@
 package httpx
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 type Request[T any] struct {
-	http.Request
-	Body T
+	*http.Request
+}
+
+// JSON attempts to decode the request body as JSON.
+func (r *Request[T]) JSON() (*T, error) {
+	body := new(T)
+	err := json.NewDecoder(r.Body).Decode(&body)
+	return body, err
 }
